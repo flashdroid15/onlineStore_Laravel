2a8568c52755389b4b477f163f8961275d07bfd6
Roshan Samuel <85951530+flashdroid15@users.noreply.github.com>
2024-09-17 02:52:51 +0000

    home folder moved correctly; Chapter 6 done

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+	public function index()
+	{
+		$viewData = [];
+		$viewData["title"] = "Home Page - Online Store";
+
+		return view('home.index')->with("viewData", $viewData);
+	}
+
+	/*
+    | home.index == onlineStore\resources\views\home\index.blade.php
+    | The . in home.index is basically \
+    */
+
+	public function about()
+	{
+		$data1 = "About us - Online Store";
+		$data2 = "About us";
+		$description = "This is an about page ...";
+
+		$author = "Developed by: Your Name";
+
+		return view('home.about')->with("title", $data1)
+			->with("subtitle", $data2)
+			->with("description", $description)
+			->with("author", $author);
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // return view('welcome');
+    /* 
+    | Above view presents the view that says "
+    | "A Laravel Online Store
+    | Welcome to the application"
+    */
+
+    $viewData = [];
+    $viewData["title"] = "Home Page - Online Store";
+
+    return view('home.index')->with("viewData", $viewData);
+    /*
+    | view() returns a view instance
+    | with() passes $viewData array to the view
+    */
 });
+
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
+// name() method defines a custom route name
